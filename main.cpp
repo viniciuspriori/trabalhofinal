88d2563a7d69742f429634a1bf7df37dfc1435a1
Vinicius Priori <82621517+viniciuspriori@users.noreply.github.com>
2021-06-28 21:44:14 +0000

    restricao de user

--- main.cpp
+++ main.cpp
@@ -1,48 +1,74 @@
 #include <iostream>
 #include <fstream>
 #include <locale.h>
+#include <string>
 #include "triangulo.h"
 
 using namespace std;
 
-int main() {
-  
-  tLista* triangulos = new tLista;
-  tTriangulo t;
-  int quant = 0;
-  int opcao = 0;
-
-do{
-  opcao = 0;
-  cout <<"No. -1: Sair\nNo. 1. Gravar \nNo. 2: Ler\n";
-  cin >> opcao;
-
-  if(opcao==1){
-    inicializaLista(triangulos);
-  
-    cout << "Digite quantos triangulos\n";
-    cin >> quant;
-    
-    for(int i = 0; i < quant; i++){
-      cout << "\nDigite os lados dos triangulos\n";
-      cin >> t.lado1 >> t.lado2 >> t.lado3;
-      incluirNoFim(triangulos, t);
+bool isInt(string str){
+    for(int i = 0; i < str.length(); i++){
+        if(!isdigit(str[i])){
+            return false;
+        }
     }
+    return true;
+}
 
-    gravaTxt(triangulos);
-    continue;
-  }
-  
-  if(opcao==2){
-    inicializaLista(triangulos);
-    cout << "Começando leitura do banco de dados. \n";
-    leTxt(triangulos);
-    imprimirLista(triangulos);
-    continue;
-  }
+int lerQuantidadeTriangulos(){
 
-}while((opcao!=-1));
+    string quantidadeTriangulos;
+    cout << "Digite a quantidade de triangulos: ";
+    cin >> quantidadeTriangulos;
 
+    if (isInt(quantidadeTriangulos)){
+        return stoi(quantidadeTriangulos);
+    }
+    else{
+        return lerQuantidadeTriangulos();
+    }
+}
+
+string lerOpcao() {
+    string opcao;
+    cout << "Digite uma das seguintes opcoes:\n1 - Gravar\n2 - Ler\n3 - Sair\n";
+    cin >> opcao;
+
+    return opcao;
+}
+
+int main(){   
+
+    tLista* triangulos = new tLista;
+    tTriangulo t; 
+    string opcao;
+
+    do{
+        opcao = lerOpcao();
+
+        if(opcao == "1"){
+            inicializaLista(triangulos);
+            int quantidadeTriangulos = lerQuantidadeTriangulos();
+
+            for (int i = 0; i < quantidadeTriangulos; i++){
+              cout << "\nDigite os lados dos triangulos\n";
+              cin >> t.lado1 >> t.lado2 >> t.lado3;
+              incluirNoFim(triangulos, t);
+            }
+              gravaTxt(triangulos);
+              continue;
+        }
+        else{
+            if (opcao == "2"){
+              inicializaLista(triangulos);
+              cout << "Começando leitura do banco de dados. \n";
+              leTxt(triangulos);
+              imprimirLista(triangulos);
+              continue;            
+              }
+        }
+
+    }while (opcao != "3");
 
   return 0;
 }
